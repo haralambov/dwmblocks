9f48f7566b850f8efe27828259b23536bac0325d
Zlatomir Haralambov <z.haralambov@gmail.com>
2021-11-09 18:44:44 +0200

    Adds keyboard layout block

--- blocks.def.h
+++ blocks.def.h
@@ -1,6 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"", "setxkbmap -query | grep -i layout | awk '{print $2}' | cut -d, -f1",	0,		10},
 	{"", "sensors | grep Core | head -n 1 | awk '{print $3}'",	2,		0},
 	{"", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	5,		0},
 	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
