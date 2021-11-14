68d77d6e9f2254b737f426deaf616ea6cdf76855
Zlatomir Haralambov <z.haralambov@gmail.com>
2021-11-14 20:11:11 +0200

    Adds volume block

--- blocks.def.h
+++ blocks.def.h
@@ -2,6 +2,7 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"", "setxkbmap -query | grep -i layout | awk '{print $2}' | cut -d, -f1",	0,		10},
+	{" ", "pactl list sinks | grep '^[[:space:]]Volume:' | head -n $(( $SINK + 1 )) | tail -n 1 | sed -e 's,.* \\([0-9][0-9]*\\)%.*,\\1,'",	0,		12},
 	{"", "sensors | grep Core | head -n 1 | awk '{print $3}'",	2,		0},
 	{"", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	5,		0},
 	{" ", "echo $(($(brightnessctl get) * 10))%",	0,		11},
